1e923024cd913fa76e5f428cf631007bb89b17b3
Phillip Lord <phillip.lord@russet.org.uk>
2020-03-22 11:14:21 +0000

    Version 0.3.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uk.org.russet/tawny-bubo "0.3.1"
+(defproject uk.org.russet/tawny-bubo "0.3.2-SNAPSHOT"
   :description "Automated Scripting Framework for Tawny-OWL"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.csv "0.1.4"]
