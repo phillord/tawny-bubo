2e2c05b23ccd8dc7beaf98db4110d6434a24bad8
Phillip Lord <phillip.lord@russet.org.uk>
2020-03-22 11:08:21 +0000

    Hopefully, update the version number in bin on release

--- project.clj
+++ project.clj
@@ -10,6 +10,20 @@
             :url "http://www.gnu.org/licenses/lgpl-3.0.txt"
             :distribution :repo}
 
+  :plugins [[lein-file-replace "0.1.0"]]
+  :release-tasks
+  [["vcs" "assert-committed"]
+   ["change" "version" "leiningen.release/bump-version" "release"]
+
+   ["file-replace" "bin/bubo" "tawny-bubo \"" "\"]" "version"]
+
+   ["vcs" "commit"]
+   ["vcs" "tag"]
+   ["deploy"]
+   ["change" "version" "leiningen.release/bump-version"]
+   ["vcs" "commit"]
+   ["vcs" "push"]]
+
   :aliases
   ;; alias test to also do install; bubo works through the command line and finds
   ;; the rest of the library from maven, so we must install before we run tests.
