ebafecf31a4ae9975a19d9dfd91fe48c04534600
Phillip Lord <phillip.lord@russet.org.uk>
2020-04-09 10:25:44 +0100

    Remove tawny-name from test

--- dev-resources/apply-csv.clj
+++ dev-resources/apply-csv.clj
@@ -3,12 +3,19 @@
 (clojure.core/use 'tawny.bubo.csv)
 
 (defontology o
-  :iri "http://example.com/o")
+  :iri "http://example.com/o"
+  :noname true)
 
 (defpattern pattern [a r b]
   (entity
       [(object-property r)
        (owl-class b)]
+    (owl-class a :super (some r b))))
+
+(clojure.core/defn pattern [a r b]
+  (clojure.core/let
+      [r (object-property r)
+       b (owl-class b)]
     (owl-class a :super (some r b))))
 
 (csv-apply
