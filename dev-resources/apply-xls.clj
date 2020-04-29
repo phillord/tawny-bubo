41762c5626837f55b33ad1130e6adf6b71710976
Aisha Blfgeh <aisha.blfgeh@gmail.com>
2020-04-29 03:35:00 +0100

    Define Excel pattern functions
    This is initial attempts to extract the Excel sheet values and convert them to OWL classes.

--- dev-resources/apply-xls.clj
+++ dev-resources/apply-xls.clj
@@ -1,19 +1,71 @@
 #!/usr/bin/env bubo
 
-(clojure.core/use 'tawny.bubo.xls)
+;(clojure.core/require 'tawny.bubo.xls)
+(cc/use 'tawny.bubo.xls)
+
 
 (defontology o
   :iri "http://example.com/o")
 
-(defpattern pattern [a r b]
-  (entity
-      [(object-property r)
-       (owl-class b)]
-    (owl-class a :super (some r b))))
+(defpattern xls-pattern
+  [a b c & {:keys [sheet orientation from to column]
+            :or {sheet "Sheet1"
+                 orintation horizontal
+                 from "A1"
+                 to "C3"
+                 column A}}]
 
+ ;; This is to convert the row data into pattern as:
+ ;; (owl-class a :super (some b c))
+
+ (entity
+   [(object-property b)
+    (owl-class c)]
+   (owl-class a :super (some b c)))
+
+;; This is to read one column and convert it into teir
+(when column
+  :orientation :vertical
+  (doall
+   (let [column-data
+         (tawny.bubo.xls/column-info sheet column column)]
+     (pattern/tier (first column-data) (rest column-data))))
+  )
+)
+
+;; (doall
+;;  (map (intern-owl-string % (owl-class %) args)))
+
+;; (cc/let [first-class (owl-class (cc/first args))
+;;       rst-class (owl-class (cc/rest args))]
+;;   (tawny.pattern/tier first-class [rest-class]
+;;     :functional false
+;;     :cover false)))
+
+;; (tawny.bubo.xls/column-classes
+;;  ;(column-inf "Sheet3" :B)
+;;  "xls-test.xlsx" "Sheet1" :A)
+
+
+;; Apply the pattern horizontally
 (xls-apply
- pattern
+ (xls-pattern :sheet "Sheet2" :orientaion :horizontal)
  "xls-test.xlsx"
- "Sheet1")
+ )
+
+;; Apply the pattern vertically on the column B in Sheet3
+(xls-apply
+ (xls-pattern :sheet "Sheet3" :column :B)
+ "xls-test.xlsx"
+ "Sheet3")
 
 (save :omn)
+
+;; This is an example of the Excel data in Sheet3:
+;;
+;; Artists/Groups     Genre      Website
+;; 1 0 AM             Pop        www.10am.bandcamp.com/
+;; Alice Ella         Pop        www.aliceella.co.uk
+;; Alina Bzhezhinska  Jazz       www.alina-harpist.com
+;; Alison Beattie     Classical  www.alisonbeattiemusic.com
+;; Alison Rayner      Jazz       www.alisonrayner.com
